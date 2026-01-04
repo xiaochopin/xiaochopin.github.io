a7d04a3b1ecc2a1601091646aee9f4e97a0191c7
chopin <48295799@qq.com>
2026-01-04 19:40:37 +0800

    修复样式表链接格式并移除无用的初始化脚本

--- assets/scripts/init-highlight-copy.js
+++
@@ -1,7 +0,0 @@
-// highlight.js 初始化后再初始化 code-copy
-hljs.initHighlightingOnLoad();
-document.addEventListener('DOMContentLoaded', function() {
-  setTimeout(function() {
-    initCodeCopy();
-  }, 0);
-});
